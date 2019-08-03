dfcf0fdec20873c04aaa6fd893ae02ac0b937e74
joanManuel <joan.torres.jt@gmail.com>
2019-08-03 11:40:58 -0500

    Arreglando menu wordpress

--- functions.php
+++ functions.php
@@ -1,18 +1,6 @@
 <?php
 /** Consultas reutilizables  **/
 //require get_template_directory().'/inc/queries.php';
-function perubroker_setup(){
-    //Habilitar imagenes destacadas
-    add_theme_support('post-thumbnails');
-
-    //Agregar imagenes de tamaño personalizado
-    add_image_size('square',350,350,true);
-    add_image_size('portrait',350,724,true);
-    add_image_size('cajas',400,375,true);
-    add_image_size('mediano',700,400,true);
-    add_image_size('blog',966,644,true);
-}
-add_action('after_setup_theme','perubroker_setup');
 
 // Menus de navegación, agregar más utilizando el arreglo
 function perubroker_menus(){
@@ -40,9 +28,18 @@ function perubroker_scripts_styles(){
 
     //wp_enqueue_script('slicknavJS',get_template_directory_uri().'/js/jquery.slicknav.min.js',array('jquery'),'1.0.0',true);
 
-    wp_enqueue_script('scripts',get_template_directory_uri().'/js/scripts.js',array('jquery'),'1.0.0',true);
-    //wp_enqueue_script('jquery-3.4.1',get_template_directory_uri().'/js/jquery-3.4.1.js',array('jquery-3.4.1'),'3.4.1',true);
+    // wp_enqueue_script('jquery','https://code.jquery.com/jquery-3.4.1.slim.min.js',array(),'3.4.1');
+    // wp_enqueue_script('jquery');
+
+    // wp_register_script('scripts',get_template_directory_uri().'/js/script.js',array(),'1.0.0',true);//true es para que lo cargue en el footer
+
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('script', get_stylesheet_directory_uri().'/js/script.js',array('jquery'),'1.0.0',true);
+    // wp_enqueue_script('scripts');
+    // wp_enqueue_script('jquery-3.4.1',get_template_directory_uri().'/js/jquery-3.4.1.js',array('jquery-3.4.1'),'3.4.1',true);
 }
 add_action('wp_enqueue_scripts','perubroker_scripts_styles');
+
+
 
 ?>
